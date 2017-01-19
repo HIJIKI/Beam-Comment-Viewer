11605b94ce521ecbd856a0c4a26f568850b25114
HIJIKI <hijiki_seaweed@yahoo.co.jp>
2017-01-20 06:26:33 +0900

    ・コメント出力処理を HTML 側のデザインに対応 ・コメント出力時に一番下まで自動でスクロールさせるように (過去ログ閲覧時は無効) ・1 秒に 1 回、ランダムなユーザーからランダムなコメントを擬似的に出力するように (デバッグ用) ・アイコン画像を変更

--- js/CommentLog.js
+++ js/CommentLog.js
@@ -1,4 +1,41 @@
-function PutComment(Text)
+//--------------------------------------------------------------------------------------------------
+//= ログにコメントを送信
+//--------------------------------------------------------------------------------------------------
+function PutComment(IconURL, UserName, Comment)
 {
-	$('#CommentLog').after('<p>'+Text+'</p>');
+	const ImgTag = '<img src="'+IconURL+'">';
+
+	// 現在時刻を取得
+	var date = new Date();
+	var Hou = ('0' + date.getHours()).slice(-2);
+	var Min = ('0' + date.getMinutes()).slice(-2);
+	var Sec = ('0' + date.getSeconds()).slice(-2);
+	var Time = Hou+":"+Min+":"+Sec;
+
+	var Data =
+	[
+		'<div class="Row">',
+		'	<div class="Icon"><span>'+ImgTag+'</span></div>',
+		'	<div class="UserName"><span>'+UserName+'</span></div>',
+		'	<div class="Time"><span>'+Time+'</span></div>',
+		'	<div class="Comment"><span>'+Comment+'</span></div>',
+		'</div>'
+	].join("");
+
+	// スクロールバーが一番下にあるか
+	var ScrollBarIsBottom = false;
+	var ViewCommentArea = $('#ViewCommentArea');
+	if( ViewCommentArea.scrollTop()+ViewCommentArea.outerHeight() == ViewCommentArea.get(0).scrollHeight )
+	{
+		ScrollBarIsBottom = true;
+	}
+
+	$('#CommentOutputPosition').before(Data);
+
+	// コメント出力前にスクロールバーが一番下にあった場合はスクロールバーを一番下に再調整
+	if( ScrollBarIsBottom )
+	{
+		ViewCommentArea.scrollTop(ViewCommentArea.get(0).scrollHeight);
+	}
+	
 }
--- js/Common.js
+++ js/Common.js
@@ -2,6 +2,11 @@
 //= Common
 //--------------------------------------------------------------------------------------------------
 
+	window.onload = function()
+	{
+		setInterval("RundomComment()",1000);
+	}
+
 	//----------------------------------------------------------------------------------------------
 	//= アバター画像のURLを取得する
 	//----------------------------------------------------------------------------------------------
@@ -11,4 +16,18 @@
 		const Suffix = "/avatar";
 		var Ret = Prefix + UserID + Suffix;
 		return Ret;
+	}
+
+	//----------------------------------------------------------------------------------------------
+	//= ランダムなコメント表示テスト (Debug)
+	//----------------------------------------------------------------------------------------------
+	function RundomComment()
+	{
+		const UserName = ["HIJIKIsw", "Bill", "Johny", "Connie Talbot", "Michael Jackson", "かずお", "たかし", "裕行", "佳輔", "多嘉山 克真", "波々伯部 達馬"];
+		const Comment = ["こんにちは。", "Hi", "Hello", "あけましておめでとうございます。", "あのー、肉じゃが作りすぎちゃったんですけど、無事食べきれました", "GG", "行く手をふさがれたら、回り道で行けばいいのよ。", "When you come to a roadblock, take a detour.", "機敏であれ、しかし慌ててはいけない。", "Be quick, but don’t hurry.", "コンピューターおばあちゃん。"];
+
+		var N = UserName[Math.floor(Math.random()*UserName.length)];
+		var C = Comment[Math.floor(Math.random()*Comment.length)];
+
+		PutComment(GetAvatarURL(703508), N, C);
 	}
